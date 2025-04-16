a8730d8ef827b11a0b277fb39dd36174ce9b3038
Jesus <113791040+016jesus@users.noreply.github.com>
2025-04-16 16:42:55 -0500

    practica 6 corregida

---
+++ Lab6 - introduccion a Java/src/labsw/Calculator.java
@@ -0,0 +1,27 @@
+package labsw;
+
+import java.util.Scanner;
+
+public class Calculator {
+
+    public double Sum(double n1, double n2){
+        return n1 + n2;
+    }
+    public double Substract(double n1, double n2){
+        return n1 - n2;
+    }
+    public double Multiply(double n1, double n2){
+        return n1 * n2;
+    }
+    public double Divide(double n1, double n2){
+        do {
+            if(n2 == 0){
+            System.out.println("No se puede dividr por cero, por favor ingrese otro numero\n");
+            System.out.print(">>");
+            Scanner entrada = new Scanner(System.in);
+            n2 = entrada.nextDouble();
+            }
+        } while (n2 == 0);
+        return n1 / n2;
+    }
+}
