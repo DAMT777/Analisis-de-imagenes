1a924fab0da9b6dde81f223fd2a41c3b5304d78c
Cesar <caperez.ochoa@gmail.com>
2025-05-25 04:16:29 -0500

    Añadido algunos controles de errores, mejorado ademas algunas vistas

---
+++ src/main/java/com/mycompany/prototiposoftware/NewUserPanelController.java
@@ -0,0 +1,79 @@
+package com.mycompany.prototiposoftware;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.HBox;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class UserPanelController {
+
+    @FXML
+    private TextField newName;
+    @FXML
+    private TextField newApellido;
+    @FXML
+    private TextField newEmail;
+    @FXML
+    private TextField newPassword;
+
+
+    @FXML
+    private AnchorPane menuBox;  // menu expandible
+
+    @FXML
+    private HBox analisisHbox;
+    @FXML
+    private HBox reportsHBox;
+    @FXML
+    private HBox userHBox;
+    @FXML
+    private HBox adminListUserHBox;
+
+    @FXML
+    private void handleHBoxClick(MouseEvent event) {
+        try {
+            // Obtener el HBox que disparó el evento
+            HBox clickedHBox = (HBox) event.getSource();
+
+            // Obtener el fx:id del HBox
+            String hboxId = clickedHBox.getId();
+
+            // Obtener el Stage actual desde el HBox
+            Stage stage = (Stage) clickedHBox.getScene().getWindow();
+
+            // Cambiar escena usando Utilities
+            changeScene.changeScene(stage, hboxId);  // Asumiendo que el archivo fxml se llama igual que el id + ".fxml"
+
+        } catch (Exception e) {
+            e.printStackTrace();
+            // Aquí puedes mostrar un mensaje al usuario si quieres
+        }
+    }
+
+    @FXML
+    private void irAdminUserList() throws IOException {
+        App.setRoot("AdminUsersList");
+    }
+
+    @FXML    //cambio de escena al hacer lcick en salir
+    private void irALoginController() throws IOException {
+        UserSesionData.clearSession();
+        App.setRoot("LoginScene");
+    }
+
+    @FXML
+    private void menuBoxExpand() {
+        menuBox.setVisible(!menuBox.isVisible());
+        menuBox.setManaged(menuBox.isVisible());
+    }
+
+    @FXML
+    private void userUpdate() {
+
+    }
+
+}
