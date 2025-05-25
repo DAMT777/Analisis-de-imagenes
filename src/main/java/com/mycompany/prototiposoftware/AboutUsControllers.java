474b5705e7e060b20cf119cb59e9d912a6f5a3a2
Cesar <caperez.ochoa@gmail.com>
2025-05-25 16:16:47 -0500

    sSi

--- src/main/java/com/mycompany/prototiposoftware/AboutUsControllers.java
+++ src/main/java/com/mycompany/prototiposoftware/AboutUsControllers.java
@@ -1,27 +1,14 @@
 package com.mycompany.prototiposoftware;
 
-import com.databaseInteractions.DBConnect;
-import javafx.collections.FXCollections;
-import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
-import javafx.fxml.Initializable;
-import javafx.scene.control.Label;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.cell.PropertyValueFactory;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.net.URL;
-import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
-import java.util.ResourceBundle;
 
-public class AboutUsController{
+public class AboutUsControllers {
     @FXML
     private AnchorPane menuBox;  // menu expandible
 
